c6aba2f7e3088a7adcdb00aaa01d3e7bea5cbdcb
BugChang <81069681@qq.com>
2018-04-25 16:35:38 +0800

    111

--- HardWareHelper/FrmMain.Designer.cs
+++ HardWareHelper/FrmMain.Designer.cs
@@ -28,21 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.axCpuCardOCX1 = new AxCPUCARDOCXLib.AxCpuCardOCX();
+            ((System.ComponentModel.ISupportInitialize)(this.axCpuCardOCX1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(778, 544);
+            this.txtLog.TabIndex = 0;
+            // 
+            // axCpuCardOCX1
+            // 
+            this.axCpuCardOCX1.Enabled = true;
+            this.axCpuCardOCX1.Location = new System.Drawing.Point(431, 216);
+            this.axCpuCardOCX1.Name = "axCpuCardOCX1";
+            this.axCpuCardOCX1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axCpuCardOCX1.OcxState")));
+            this.axCpuCardOCX1.Size = new System.Drawing.Size(100, 50);
+            this.axCpuCardOCX1.TabIndex = 1;
+            this.axCpuCardOCX1.Visible = false;
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 392);
+            this.ClientSize = new System.Drawing.Size(778, 544);
+            this.Controls.Add(this.axCpuCardOCX1);
+            this.Controls.Add(this.txtLog);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmMain";
             this.Text = "硬件助手";
             this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axCpuCardOCX1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private AxCPUCARDOCXLib.AxCpuCardOCX axCpuCardOCX1;
     }
 }
--- HardWareHelper/FrmMain.cs
+++ HardWareHelper/FrmMain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.IO;
 using System.IO.Ports;
 using System.Net;
@@ -7,17 +8,15 @@ using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
-using AxCPUCARDOCXLib;
 using Newtonsoft.Json;
 
 namespace HardWareHelper
 {
     public partial class FrmMain : Form
     {
-
-        private static SerialPort _serialPort;
-        private static string _comData;
-        private static AxCpuCardOCX axCpuCard = new AxCpuCardOCX();
+        private HttpListener _httpListener;
+        private SerialPort _serialPort;
+        private string _comData;
 
         public FrmMain()
         {
@@ -26,7 +25,11 @@ namespace HardWareHelper
 
         private void FrmMain_Load(object sender, EventArgs e)
         {
-            StartListen();
+
+            if (StartListen())
+            {
+                WriteLog("Start listening at  http://localhost:8181/");
+            }
         }
 
         /// <summary>
@@ -35,33 +38,62 @@ namespace HardWareHelper
         /// <returns></returns>
         public bool StartListen()
         {
-            HttpListener httpListener = new HttpListener
+            try
             {
-                AuthenticationSchemes = AuthenticationSchemes.Anonymous
-            };
-
-            httpListener.Prefixes.Add("http://localhost:8181/");
-            httpListener.Start();
-            new Thread(new ThreadStart(delegate
-            {
-                while (true)
+                _httpListener = new HttpListener
                 {
+                    AuthenticationSchemes = AuthenticationSchemes.Anonymous
+                };
 
-                    HttpListenerContext httpListenerContext = httpListener.GetContext();
-                    httpListenerContext.Response.StatusCode = 200;
-                    if (httpListenerContext.Request.Url.LocalPath.Contains("."))
+                _httpListener.Prefixes.Add("http://localhost:8181/");
+                _httpListener.Start();
+                _httpListener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
+                _httpListener.UnsafeConnectionNtlmAuthentication = false;
+                _httpListener.BeginGetContext(GetContextCallBack, _httpListener);
+                var isListening = _httpListener.IsListening;
+                return isListening;
+            }
+            catch (Exception e)
+            {
+                WriteLog(e.Message);
+                return false;
+            }
+        }
+
+        void GetContextCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                _httpListener = ar.AsyncState as HttpListener;
+                if (_httpListener != null)
+                {
+                    _httpListener.BeginGetContext(GetContextCallBack, _httpListener);
+                    if (_httpListener != null)
                     {
-                        continue;
-                    }
-                    using (StreamWriter writer = new StreamWriter(httpListenerContext.Response.OutputStream))
-                    {
-                        writer.Write(ProcessingHandle(httpListenerContext));
+                        HttpListenerContext httpListenerContext = _httpListener.EndGetContext(ar);
+                        //过滤资源类路径
+                        if (httpListenerContext.Request.Url.ToString().Contains("."))
+                        {
+                            return;
+                        }
+                        httpListenerContext.Response.StatusCode = 200;
+                        httpListenerContext.Response.AddHeader("Access-Control-Allow-Origin", "*");
+                        using (StreamWriter writer = new StreamWriter(httpListenerContext.Response.OutputStream))
+                        {
+                            WriteLog("request：" + httpListenerContext.Request.Url);
+
+                            var responseText = ProcessingHandle(httpListenerContext);
+                            writer.Write(responseText);
+                            WriteLog("response：" + responseText);
+                        }
                     }
                 }
-            })).Start();
+            }
+            catch (Exception exception)
+            {
+                WriteLog(exception.Message);
+            }
 
-            var isListening = httpListener.IsListening;
-            return isListening;
         }
 
         /// <summary>
@@ -77,7 +109,7 @@ namespace HardWareHelper
             {
                 return "不存在的方法，请检查Url拼写！";
             }
-            var result = method.Invoke(null, new object[] { httpListenerContext });
+            var result = method.Invoke(this, new object[] { httpListenerContext });
             return JsonConvert.SerializeObject(result);
         }
 
@@ -85,7 +117,7 @@ namespace HardWareHelper
         /// 获取Mac地址
         /// </summary>
         /// <returns></returns>
-        public static IList<string> GetMacAddresses(HttpListenerContext httpListenerContext)
+        public IList<string> GetMacAddresses(HttpListenerContext httpListenerContext)
         {
             var macAddresses = new List<string>();
             NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
@@ -99,13 +131,23 @@ namespace HardWareHelper
             return macAddresses;
         }
 
+        public IList<string> GetPrinters(HttpListenerContext httpListenerContext)
+        {
+            List<string> printers = new List<string>();
+            foreach (string printer in PrinterSettings.InstalledPrinters)
+            {
+                printers.Add(printer);
+            }
+            return printers;
+        }
+
         #region 扫描枪&读卡器
 
         /// <summary>
         /// 获取串口名称列表
         /// </summary>
         /// <returns></returns>
-        public static string[] GetSerialPortNames(HttpListenerContext httpListenerContext)
+        public string[] GetSerialPortNames(HttpListenerContext httpListenerContext)
         {
             string[] serialPortNames = SerialPort.GetPortNames();
             return serialPortNames;
@@ -116,10 +158,17 @@ namespace HardWareHelper
         /// </summary>
         /// <param name="httpListenerContext"></param>
         /// <returns></returns>
-        public static bool OpenSerialPort(HttpListenerContext httpListenerContext)
+        public bool OpenSerialPort(HttpListenerContext httpListenerContext)
         {
             try
             {
+                if (_serialPort != null)
+                {
+                    if (_serialPort.IsOpen)
+                    {
+                        _serialPort.Close();
+                    }
+                }
                 var portName = httpListenerContext.Request.QueryString["portName"];
                 var baudRate = Convert.ToInt32(httpListenerContext.Request.QueryString["baudRate"]);
                 _serialPort =
@@ -133,7 +182,7 @@ namespace HardWareHelper
             }
             catch (Exception e)
             {
-                MessageBox.Show(e.Message);
+               WriteLog(e.Message);
                 return false;
             }
             return true;
@@ -144,7 +193,7 @@ namespace HardWareHelper
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private static void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        private void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             Thread.Sleep(500);//延迟500ms，防止有些硬件设备未写完数据
             var text = _serialPort.ReadExisting();
@@ -155,19 +204,21 @@ namespace HardWareHelper
         /// 获取串口数据
         /// </summary>
         /// <returns></returns>
-        public static string GetComData(HttpListenerContext httpListenerContext)
+        public string GetComData(HttpListenerContext httpListenerContext)
         {
             while (string.IsNullOrEmpty(_comData))
             {
                 Thread.Sleep(100);
             }
-            return _comData;
+            var returnData = _comData;
+            _comData = "";
+            return returnData;
         }
 
         /// <summary>
         ///关闭串口
         /// </summary>
-        public static void CloseSerialPort(HttpListenerContext httpListenerContext)
+        public void CloseSerialPort(HttpListenerContext httpListenerContext)
         {
             _serialPort.Close();
         }
@@ -178,36 +229,38 @@ namespace HardWareHelper
 
         #region 国办CPU卡
 
-        public static bool WriteCpuCard(HttpListenerContext httpListenerContext)
+        public bool WriteCpuCard(HttpListenerContext httpListenerContext)
         {
             var port = Convert.ToInt16(httpListenerContext.Request.QueryString["port"]);
             var baud = Convert.ToInt32(httpListenerContext.Request.QueryString["baudRate"]);
             byte[] data = Encoding.UTF8.GetBytes(httpListenerContext.Request.QueryString["data"]);
             string dataBase64String = Convert.ToBase64String(data);
-            axCpuCard.ClosePort();
-            var bOpen = axCpuCard.OpenPort(port, baud);
+            axCpuCardOCX1.ClosePort();
+            var bOpen = axCpuCardOCX1.OpenPort(port, baud);
+            Thread.Sleep(500);
             if (bOpen)
             {
-                if (axCpuCard.IsCardAvailabile)
+                if (axCpuCardOCX1.IsCardAvailabile)
                 {
-                    return axCpuCard.SetFileDataBinBase64(dataBase64String);
+                    return axCpuCardOCX1.SetFileDataBinBase64(dataBase64String);
                 }
             }
             return false;
         }
 
 
-        public static string ReadCpuCard(HttpListenerContext httpListenerContext)
+        public string ReadCpuCard(HttpListenerContext httpListenerContext)
         {
             var port = Convert.ToInt16(httpListenerContext.Request.QueryString["port"]);
             var baud = Convert.ToInt32(httpListenerContext.Request.QueryString["baudRate"]);
-            axCpuCard.ClosePort();
-            var bOpen = axCpuCard.OpenPort(port, baud);
+            axCpuCardOCX1.ClosePort();
+            var bOpen = axCpuCardOCX1.OpenPort(port, baud);
+            Thread.Sleep(500);
             if (bOpen)
             {
-                if (axCpuCard.IsCardAvailabile)
+                if (axCpuCardOCX1.IsCardAvailabile)
                 {
-                    return Encoding.UTF8.GetString(Convert.FromBase64String(axCpuCard.GetFileDataBinBase64()));
+                    return Encoding.UTF8.GetString(Convert.FromBase64String(axCpuCardOCX1.GetFileDataBinBase64()));
                 }
             }
             return "";
@@ -215,5 +268,21 @@ namespace HardWareHelper
 
         #endregion
 
+
+        private delegate void WriteLogDelegate(string message);
+        /// <summary>
+        /// 记录日志
+        /// </summary>
+        /// <param name="message"></param>
+        private void WriteLog(string message)
+        {
+            if (txtLog.InvokeRequired == false)
+                txtLog.AppendText(DateTime.Now + "|" + message + "\r\n");
+            else
+            {
+                var writeLogDelegate = new WriteLogDelegate(WriteLog);
+                txtLog.Invoke(writeLogDelegate, message);
+            }
+        }
     }
 }
